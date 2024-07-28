44424a415236c0f5e44c83ecd070d1b017506d56
albbloomer <a_l_b_bloomer@naver.com>
2024-07-28 13:57:53 +0900

    refactor : user name, email 수정

--- dart_application/bin/dart_application.dart
+++ dart_application/bin/dart_application.dart
@@ -1,5 +1,6 @@
 import 'package:dart_application/dart_application.dart' as dart_application;
 
 void main(List<String> arguments) {
+  print('Hi zero and minyul');
   print('Hello world: ${dart_application.calculate()}!');
 }
